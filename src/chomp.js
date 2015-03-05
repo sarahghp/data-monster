c62fc285100c8521126c6dccea9ee02cf8ecfbb2
Sarah GP <sarah.groff.palermo@gmail.com>
2015-03-05 15:35:12 -0500

    Check for file type

--- src/chomp.js
+++ src/chomp.js
@@ -4,17 +4,30 @@ var fs = require('fs');
 function compile(){
 
   var file = process.argv[2],
-      word = fs.readFileSync(file),
-      title = file.slice(0, -3),
-      outputTitle = title + '.js';
+      word = fs.readFileSync(file);
+
+
+  // Check that the file is type data-monster
+
+  if (file.slice(-3) !== '.dm'){
+    throw "I'm sorry but I can only chomp data-monster (.dm) files";
+  }
+
+  var title = file.slice(0, -3),      // remove .dm
+    outputTitle = title + '.js';    // add .js
+
+
+  // Then do things to file
 
   var output = (function addCat(word){
     return word + 'Cat';
   })(word);
 
+  // Then write out js file
+
   fs.writeFile(outputTitle, output, function(err){
     if(err) throw err;
-    console.log('Completed');
+    console.log(' Nom nom the data monster has chomped ' + file + '\n\n v   V  \n  ^ ^ \n\n');
   });
 
 }
