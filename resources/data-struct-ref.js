40b116b61feadb6fc3815de181b764f1f4f0a529
Sarah GP <sarah.groff.palermo@gmail.com>
2015-05-15 14:00:00 -0400

    Update example for new interpreter file

--- resources/data-struct-ref.js
+++ resources/data-struct-ref.js
@@ -1,118 +1,62 @@
-// generate [keys] using uuid gen
+// Example — Each array is generated from a code block / containing expression
 
-{
-
-  [data]: {
-    file:
-    filetype:
-    clean: [ ] // array of funcs
-    children: [ ] // refs to children
-  }
-
-  [canvas]: {
-    selector:
-    height:
-    width:
-    margins:
-    color:
-    xScale: { 
-      scale:
-      range: 
-      domain: }
-    yScale: { 
-      scale:
-      range: 
-      domain: }
-    xAxis:  { 
-      scale:          // these will have defualts if not populated here
-      orientation:    // these will have defualts if not populated here
-      [other_specs] } // attr, style, &c., each is its own 
-    yAxis:  { 
-      scale:          // these will have defualts if not populated here
-      orientation:    // these will have defualts if not populated here
-      [other_specs] } // attr, style, &c., each is its own 
-    parent: // is a data, named
-    children: [ ] // refs to children
-  }
-  
-  [svg_elem]: {
-  // generate name based on selected node // clashes throw errors
-    type:
-    req_specs: { } // an object with the required svg properties, eg {'cx': 'ratio', 'cy': 'Shape_Count' }
-    [opt_specs] // attr, style, &c., each is its own
-    parent: // is a canvas, named
-    children: [ ] // refs to children
-  }
-         
-
-  special: {
-    tooltips: [{
-      text:
-      parent: 
-    }]
-  }
-}
-
-// Example
-
-{ 'data-eb544ee9-5824-4725-8e18-dc88d39fa109':
-   { file: 'van_gogh_additional_measurements.tsv',
-     filetype: '.tsv',
-     clean: [Function],
-     funcs: [ [Function: tiger], [Function: kitty] ],
-     children:
-      [ 'canvas-8818f339-43eb-438c-9d82-3fc3a940abba',
-        'canvas-ea8ea461-989c-4826-8ff7-e80d156d1e7c' ] },
-  'canvas-8818f339-43eb-438c-9d82-3fc3a940abba':
-   { parent: 'data-eb544ee9-5824-4725-8e18-dc88d39fa109',
-     width: 1000,
-     height: 600,
-     margins: { short_params: [ '20', '20', '60', '60' ] },
-     selector: '#scatterplot',
-     color: [ { variable: 'category10' } ],
-     xScale:
-      { scale: { variable: 'log' },
-        domain: { short_params: [ '0', 'height' ] },
-        range: { short_params: [ '200', '400' ] } },
-     children:
-      [ 'elem-47dacdf9-6ad6-44a8-9c69-de845166a891',
-        'elem-047d1ef3-92ce-4679-a377-ec4378ac4e9d' ],
-     xPrim: 'd.ratio',
-     yPrim: 'd.Shape_Count',
-     xAxis:
-      { attr:
-         [ [ 'class', 'label' ],
-           [ 'x', { variable: 'width' } ],
-           [ 'y', 50 ] ],
-        style: [ [ 'text-anchor', 'end' ] ],
-        text: [ 'Height: Width Ratio' ] },
-     yAxis:
-      { attr: [ [ 'class', 'label' ], [ 'y', -10 ] ],
-        style: [ [ 'text-anchor', 'end' ] ],
-        text: [ 'Num Shapes' ] } },
-  'elem-47dacdf9-6ad6-44a8-9c69-de845166a891':
-   { parent: 'canvas-8818f339-43eb-438c-9d82-3fc3a940abba',
-     type: 'circle',
-     req_specs: { cx: [Function], cy: [Function], r: 4, fill: [Function] },
-     attr: [ [ 'class', 'dot' ] ],
-     tooltip:
-      { text: 'default',
-        parent: 'elem-47dacdf9-6ad6-44a8-9c69-de845166a891' },
-     click: [Function] },
-  'elem-047d1ef3-92ce-4679-a377-ec4378ac4e9d':
-   { parent: 'canvas-8818f339-43eb-438c-9d82-3fc3a940abba',
-     type: 'line',
-     req_specs: { x1: 0, y1: 100, x2: { variable: 'width' }, y2: 100 } },
-  'canvas-ea8ea461-989c-4826-8ff7-e80d156d1e7c':
-   { parent: 'data-eb544ee9-5824-4725-8e18-dc88d39fa109',
-     width: 100,
-     height: 60,
-     margins: { short_params: [ '20', '20', '60', '60' ] },
-     selector: '#booooop',
-     children: [ 'elem-08b6b0fe-3c07-46cf-a194-a2fe23c2aa9d' ],
-     xPrim: 'd.ratio',
-     yPrim: 'd.Shape_Count' },
-  'elem-08b6b0fe-3c07-46cf-a194-a2fe23c2aa9d':
-   { parent: 'canvas-ea8ea461-989c-4826-8ff7-e80d156d1e7c',
-     type: 'circle',
-     req_specs: { cx: [Function], cy: [Function], r: 4, fill: [Function] } } }
+[ [ { name: 'data_a736e5ac',
+      file: 'van_gogh_additional_measurements.tsv',
+      filetype: '.tsv',
+      children: [ 'canvas_25782860' ] },
+    { clean: { function: 'd.Shape_Count = +d.Shape_Count; d.ratio = +d["Image_Height/Image_Width "]' },
+      parent: 'data_a736e5ac' },
+    { funcs: [ { function: 'function tiger(){return \'tiger\'}' } ],
+      parent: 'data_a736e5ac' },
+    { name: 'canvas_25782860',
+      parent: 'data_a736e5ac',
+      width: 1000,
+      height: 600,
+      margins: { short_params: [ '20', '20', '60', '60' ] },
+      selector: '#scatterplot',
+      children: [ 'elem_2ac44c58', 'elem_91b9f2ad' ] },
+    { color: [ { variable: 'category10' } ],
+      parent: 'canvas_25782860' },
+    { funcs: [ { function: 'function kitty(){return \'kitty\'}' } ],
+      parent: 'canvas_25782860' },
+    { xScale:
+       { parent: 'canvas_25782860',
+         undefined: { variable: 'linear' },
+         domain: [ { short_params: [ '0', 'maxX' ] } ],
+         range: [ { short_params: [ '0', 'width' ] } ] } },
+    { name: 'elem_2ac44c58',
+      parent: 'canvas_25782860',
+      type: 'circle',
+      req_specs:
+       { cx: { variable: 'd.ratio' },
+         cy: { variable: 'd.Shape_Count' },
+         r: 4,
+         fill: { variable: 'd.Year' } },
+      xPrim: 'd.ratio',
+      yPrim: 'd.Shape_Count' },
+    { attr: [ [ 'class', 'dot' ] ], parent: 'canvas_25782860' },
+    { tooltips: [ 'true' ], parent: 'canvas_25782860' },
+    { click: [ { function: 'function(d) { window.open(\'https://www.google.com/search?site=imghp&tbm=isch&q=van+gogh+\'+d.Title); }' } ],
+      parent: 'canvas_25782860' },
+    { xAxis:
+       { parent: 'canvas_25782860',
+         attr:
+          [ [ 'class', 'label' ],
+            [ 'x', { variable: 'width' } ],
+            [ 'y', 50 ] ],
+         style: [ [ 'text-anchor', 'end' ] ],
+         text: [ 'Height: Width Ratio' ] } },
+    { yAxis:
+       { parent: 'canvas_25782860',
+         attr: [ [ 'class', 'label' ], [ 'y', -10 ] ],
+         style: [ [ 'text-anchor', 'end' ] ],
+         text: [ 'Num Shapes' ] } },
+    { 'xAxis.ticks': [ 10 ], parent: 'canvas_25782860' },
+    { '.tickFormat': [ '%d' ], parent: 'canvas_25782860' },
+    { name: 'elem_91b9f2ad',
+      parent: 'canvas_25782860',
+      type: 'line',
+      req_specs: { x1: 0, y1: 100, x2: { variable: 'width' }, y2: 100 },
+      xPrim: undefined,
+      yPrim: undefined },
+    { 'svg.append': [ 'g' ], parent: 'canvas_25782860' } ] ]
