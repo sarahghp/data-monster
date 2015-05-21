19a29a30512d909b6a24630732af5b167bc820d3
Sarah GP <sarah.groff.palermo@gmail.com>
2015-05-21 17:43:39 -0400

    eatVars & related implemented

--- data-monster/lib/writer.js
+++ data-monster/lib/writer.js
@@ -11,6 +11,7 @@ function buildString(structure){
 
   // Lists
   var noms = {
+    // special assemblies
     data   : '',
     canvas : '',
     elem   : '',
@@ -18,44 +19,90 @@ function buildString(structure){
     yAxis  : '',
     xScale : '',
     yScale : '',
+   // // special processes 
    //  attr   : attrBite,
    //  style  : styleBite,
    //  tooltip: ttBite,
 
-   // // events <- add more, consider method to take other DOM methods
+   // // shorthand events
    //  click     :   function(args){ return eventBite(args)('click')},
    //  mouseover :   function(args){ return eventBite(args)('mouseover')},
    //  mouseenter:   function(args){ return eventBite(args)('mouseenter')},
    //  mouseleave:   function(args){ return eventBite(args)('mouseleave')},
    //  hover     :   function(args){ return eventBite(args)('hover')},
+   //  
+   //  process functions
+   variable   : eatVars,
+   'function' : eatFuncs
   },
 
   d3things = {
     // colors
-    category10  : 'pre',
-    category20  : 'pre',
-    category20b : 'pre',
-    category20c : 'pre',
+    category10  : _.partial(assembled3things, 'pre', 'scale'),
+    category20  : _.partial(assembled3things, 'pre', 'scale'),
+    category20b : _.partial(assembled3things, 'pre', 'scale'),
+    category20c : _.partial(assembled3things, 'pre', 'scale'),
 
     // axes
-    linear      : 'pre',
-    log         : 'pre',
-    identity    : 'pre',
-    sqrt        : 'pre',
-    pow         : 'pre',
-    quantize    : 'pre',
-    quantile    : 'pre',
-    threshold   : 'pre',
-    time        : 'post'
+    linear      : _.partial(assembled3things, 'pre', 'scale'),
+    log         : _.partial(assembled3things, 'pre', 'scale'),
+    identity    : _.partial(assembled3things, 'pre', 'scale'),
+    sqrt        : _.partial(assembled3things, 'pre', 'scale'),
+    pow         : _.partial(assembled3things, 'pre', 'scale'),
+    quantize    : _.partial(assembled3things, 'pre', 'scale'),
+    quantile    : _.partial(assembled3things, 'pre', 'scale'),
+    threshold   : _.partial(assembled3things, 'pre', 'scale'),
+    time        : _.partial(assembled3things, 'post', 'scale')
 
   };
 
+  // SMALL FUNCS
+  
+  function assembled3things(director, label, itself){
+    if(director === 'pre'){
+      return 'd3.' + label + '.' + itself;
+    } else if (director === 'post'){
+      return 'd3.' + itself + '.' + label;
+    } else {
+      throw new Error('Cannot assemble d3things; unknown director.');
+    }
+  }
+
+  function lookup(toFind, scope){
+    console.log('lookup called', toFind, scope);
+    var lookat = _.filter(structure, function(f){
+      return _.includes(_.keys(f), 'parent') && f.parent === 'scope';
+    });
+
+    var val = _.findLast(lookat, function(n){
+      return _.inclues(_.keys(n), toFind);
+    });
+
+    if (val) return val;
+    return lookup(toFind, _.findWhere(structure, { name: scope }));
+    // find everything with the same parent
+    // do any contain what you are looking for in the keys? if yes, return val
+    // if not, call again, passing grandparent as scope
+    // if no grandparent, error
+  }
+
+  // PROCESS FUNCS
+  
+  function eatVars(varObj, parent){
+    return _.includes(_.keys(d3things), varObj.variable) ?
+      d3things[varObj.variable](varObj.variable)
+      : lookup(varObj.variable, parent);
+  }
+
+  function eatFuncs(funcObj, parent){
+    // console.log('eatFuncs', funcObj, parent);
+  }
 
   // WORKHORSE FUNCS
   
-  function process(value){
+  function process(value, parent){
     guts.isHashMap(value) ?
-        console.log('hm')
+        console.log(noms[_.keys(value)](value, parent))
       : console.log(value);
   }
 
@@ -67,7 +114,7 @@ function buildString(structure){
              noms[exp.name.split('_')[0]]
            : _.includes(_.keys(noms), key) ?
              noms[key]
-           : key + "(" + process(exp[key]) + ")"
+           : key + "(" + process(exp[key], exp.parent) + ")"
       } else {
         throw new Error('Invalid input:' + exp);
       }
